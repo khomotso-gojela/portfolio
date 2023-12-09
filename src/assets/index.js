25821b9e4897f61468d9a7c701a833829220e9bb
khomotso gojela <motsoak49@gmail.com>
2023-12-09 22:53:03 +0200

    final portfolio

--- src/assets/index.js
+++ src/assets/index.js
@@ -19,23 +19,14 @@ import reactjs from "./tech/reactjs.png";
 import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
-import threejs from "./tech/threejs.svg";
 import live from "./live.png"
 import selenium from "./tech/selenium.png";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
 import codespace from "./company/codespace.png";
 
-
-import carrent from "./carrent.png";
 import kitchen from "./kitchen.png";
 import book from "./book.png";
 import podcast from "./podcast.png";
-import jobit from "./jobit.png";
-import tripguide from "./tripguide.png";
 
 export {
   logo,
@@ -58,14 +49,6 @@ export {
   redux,
   tailwind,
   typescript,
-  threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
-  carrent,
-  jobit,
-  tripguide,
   live,
   kitchen,
   book,
