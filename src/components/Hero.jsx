0ecb588f3fe931cac5712735532e62d57a4a9588
khomotso gojela <motsoak49@gmail.com>
2023-12-09 23:01:42 +0200

    removed 3d model

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
     <section 
       className="relative w-full h- h-screen mx-auto"
     >
-      <ComputersCanvas />
+      {/* <ComputersCanvas /> */}
       <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7x1 
         mx-auto flex flex-row items-start gap-5`}>
           <div className="flex flex-col justify-center items-center mt-5">
