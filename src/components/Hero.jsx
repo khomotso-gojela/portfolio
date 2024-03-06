d1213c724c348f5c5b2571e8c173c0fe4d78925c
khomotso gojela <motsoak49@gmail.com>
2024-03-06 21:41:54 +0200

    top

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,9 +4,9 @@ import { styles } from "../style"
 const Hero = () => {
   return (
     <section 
-      className="relative w-full h- h-screen mx-auto"
+      className="relative w-full h-screen mx-auto"
     >
-      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7x1 
+      <div className={`${styles.paddingX} absolute inset-0 top-[170px] max-w-7x1 
         mx-auto flex flex-row items-start gap-5`}>
           
           <div>
