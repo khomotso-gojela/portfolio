b0f09e7f8a4b2a671e6f4812e8d25ce59b15c901
khomotso gojela <motsoak49@gmail.com>
2024-03-17 09:31:46 +0200

    about me update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -47,9 +47,28 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className='mt-4 text-[#CADCFC] text-[17px] max-w-3xl leading-[30px]'
       >
-        I am a dynamic programmer with proficiency in ReactJS. With a passion for problem-solving and a commitment 
-        to delivering high-quality software solutions, I have established a strong 
-        foundation in software development.
+        <p>Embarking on a journey sparked by my fascination with the dynamic landscape of technology, I found my passion at the intersection of innovation and creation. 
+        It all began during my formative years in high school, where the enigmatic world of video game development captivated my imagination. Driven by an 
+        insatiable curiosity, I immersed myself in learning the intricacies of game development through self-guided tutorials and YouTube 
+        resources, fueled by a fervent desire to contribute to this thriving industry.
+        </p>
+        <br />
+        <p>
+        As my journey unfolded, I realized the importance of a solid foundation in computer science to truly manifest my aspirations. 
+        This realization propelled me towards higher education, where I diligently honed my skills in programming languages, algorithms, 
+        and the art of crafting elegant and efficient code. Armed with this knowledge, I ventured further into the realm of 
+        software development, recognizing it as a fertile ground to cultivate my programming prowess while inching closer to 
+        my dream of game development.
+        </p>
+        <br />
+        <p>
+        Today, I proudly stand as a seasoned web software tester and proficient MERN stack software developer, adept in navigating the 
+        complexities of modern web applications. My expertise extends to database modeling utilizing NoSQL, a testament to my commitment 
+        to mastering diverse technologies and methodologies. With a firm grasp on industry best practices and a relentless pursuit of 
+        excellence, I am poised to make meaningful contributions to the ever-evolving landscape of technology.
+        Driven by a relentless pursuit of excellence and fueled by a boundless passion for innovation, I am eager to continue pushing 
+        the boundaries of what is possible in the world of technology, leaving an indelible mark on the digital frontier.
+        </p>
       </motion.p>
 
       <div className='mt-20 flex flex-wrap gap-10'>
