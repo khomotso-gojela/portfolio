f1c95f36a9fd8fecd2f53cbcc908a713a26f6910
khomotso gojela <motsoak49@gmail.com>
2024-03-21 17:54:44 +0200

    changed mobile display

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,7 +15,7 @@ const Hero = () => {
             
             <p className={`${styles.heroSubText} mt-2`}> A MERN stack software developer.</p>
             
-            <div className="flex flex-col sm:flex-row items-center">
+            <div className="flex flex-col sm:flex-row items-left justify-start">
               <a href="https://drive.google.com/file/d/13-omN1k2KMC7tBsnkFcjtacfKFIByX_O/view?usp=drive_link" target="_blank">
                 <button 
                   className="bg-blue-500 text-blue-100 px-8 py-4 my-6 font-bold
@@ -24,10 +24,10 @@ const Hero = () => {
                   Download resume
                 </button>
               </a>
-              <div className="ml-8 flex items-center">
+              <div className="sm:ml-12 flex items-center mt-4">
                 {/* <a className="text-slate-600 px-4 text-3xl" href="https" target='-blank'>|</a> */}
-                <a className="text-gray-600 px-4" href="https://www.linkedin.com/in/khomotso-gojela-149995216/" target='-blank'><FaLinkedin size={24}/></a>
-                <a className="text-gray-600 px-4" href="https://github.com/khomotso-gojela" target='-blank'><FaGithub size={24}/></a>
+                <a className="text-gray-600 mr-4" href="https://www.linkedin.com/in/khomotso-gojela-149995216/" target='-blank'><FaLinkedin size={24}/></a>
+                <a className="text-gray-600 mr-4" href="https://github.com/khomotso-gojela" target='-blank'><FaGithub size={24}/></a>
               </div>
 
             </div>
