b73299c60d0686e204b93bfad0ae47b4068ee291
khomotso gojela <motsoak49@gmail.com>
2024-04-16 15:58:53 +0200

    changed alignment

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -18,68 +18,53 @@ const ProjectCard = ({
   source_code_link,
 }) => {
   return (
-    <motion.div onClick={() => window.open(source_code_link, "_blank")} variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
-      <Tilt
-        options={{
-          max: 10,
-          scale: 1,
-          speed: 450,
-        }}
-        className='bg-gray-300 sm:w-12/12 w-full rounded-md overflow-hidden flex flex-col md:flex-row shadow-lg shadow-gray-500'
-      >
-        <div className='relative w-full h-[230px] '>
-          <img
-            src={image}
-            alt='project_image'
-            className='w-full h-full object-cover object-top'
-            
-          />
+    <div
+      onClick={() => window.open(source_code_link, "_blank")} 
+      className=" cursor-pointer w-full flex flex-col sm:flex-row bg-gray-300
+         rounded-md overflow-hidden shadow-lg"
+    
+    >
+      
+      <div className='relative sm:w-3/12'>
+        <img
+          src={image}
+          alt='project_image'
+          className='w-full h-40 object-cover object-top
+            sm:w-40 sm:h-full'
+          
+        />
+      </div>
 
-          <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
-            <div
-              onClick={() => window.open(source_code_link, "_blank")}
-              className='bg-gray-200 w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
+      <div className=' p-4 items-start justify-start sm:w-9/12'>
+        <h3 className='text-blue-400 font-bold text-[24px]'>{name}</h3>
+        <p className='mt-2 text-gray-600 text-[14px] text-wrap'>{description}</p>
+        <div className='mt-4 flex flex-wrap gap-2'>
+          {tags.map((tag) => (
+            <p
+
+              key={`${name}-${tag.name}`}
+              className={`text-[14px] ${tag.color}`}
             >
-              <img
-                src={live}
-                alt='source code'
-                className='w-7 h-7 object-contain'
-              />
-            </div>
-          </div>
+              #{tag.name}
+            </p>
+          ))}
         </div>
+      </div>
 
-        <div className=' p-5'>
-          <h3 className='text-blue-400 font-bold text-[24px]'>{name}</h3>
-          <p className='mt-2 text-gray-600 text-[14px]'>{description}</p>
-          <div className='mt-4 flex flex-wrap gap-2'>
-            {tags.map((tag) => (
-              <p
-
-                key={`${name}-${tag.name}`}
-                className={`text-[14px] ${tag.color}`}
-              >
-                #{tag.name}
-              </p>
-            ))}
-          </div>
-        </div>
-
-      </Tilt>
-    </motion.div>
+    </div>
   );
 };
 
 const Works = () => {
   return (
     <>
-      <motion.div variants={textVariant()}>
+      <div variants={textVariant()}>
         <p className={`${styles.sectionSubText} `}>My work</p>
         <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
-      </motion.div>
+      </div>
 
       <div className='w-full flex'>
-        <motion.p
+        <p
           variants={fadeIn("", "", 0.1, 1)}
           className='mt-3 text-gray-500 text-[17px] max-w-3xl leading-[30px]'
         >
@@ -88,10 +73,10 @@ const Works = () => {
           links to code repositories and live demos in it. It reflects my
           ability to solve complex problems, work with different technologies,
           and manage projects effectively.
-        </motion.p>
+        </p>
       </div>
 
-      <div className='mt-20 flex flex-wrap gap-7'>
+      <div className='mt-20 flex flex-wrap gap-7 lg:grid lg:grid-cols-2 xl:grid-cols-3'>
         {projects.map((project, index) => (
           <ProjectCard key={`project-${index}`} index={index} {...project} />
         ))}
