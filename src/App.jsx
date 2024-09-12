5805c99ae34774e8300ce00ea87fa82ebc124e21
khomotso gojela <motsoak49@gmail.com>
2024-09-12 17:47:15 +0200

    removed about section

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
           <Navbar />
         </Headroom>
         <Hero />
-        <About />
+        {/* <About /> */}
         <Tech/>
         {/* <Experience /> */}
         {/* <Works/> */}
